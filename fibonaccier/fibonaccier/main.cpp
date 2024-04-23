f57c2c60574a30ba1040c57f28cecbb6a5f4fb71
Manoj <manz.karki@gmail.com>
2024-04-23 15:14:18 +0300

    Add async call functionality and define two threads to calculate fiboncci numbers.

--- fibonaccier/fibonaccier/main.cpp
+++ fibonaccier/fibonaccier/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <thread>
 
+/*
+This function calculates the value of fibonacci series for given number n using recursive function.
+*/
 uint64_t fibonacci(int n) {
     if (n <= 1) {
         return n;
@@ -7,6 +11,16 @@ uint64_t fibonacci(int n) {
     else {
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
+}
+
+/*
+This function inserts a random delay between 0 and 1 seconds then calcuates the fibonacci series for given number n.
+*/
+int random_delay_in_fibonacci(int n, char c) {
+    uint64_t result = fibonacci(n);
+    // add delay part later
+    std::cout << "fibonacci(" << n << ") = " << result << " and execution is for character " << c << " \n";  
+    return result;
 }
 
 int main() {
@@ -17,6 +31,14 @@ int main() {
     if (n <= 1) {
         std::cout << "Enter a number greater than 1. " << std::endl;
     }
+
+    //Define two threads for async calls
+    std::thread thread1(random_delay_in_fibonacci, n, '*');
+    std::thread thread2(random_delay_in_fibonacci, n, '$');
+
+    // Wait for both threads to finish
+    thread1.join();
+    thread2.join();
 
     std::cout << "Fibonacci number for the given number " << n << " is: " << fibonacci(n) << std::endl;
 
